8dc19f0328804f7355456862fbd129017bb8e474
UndefinedYass <dir16cat17@gmail.com>
2022-07-18 22:05:54 +0100

    Renamed project scraper.Interfaces to scraper.Core

---
+++ scraper.Intefaces/Enums.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace scraper.Core
+{
+    public enum FieldRole
+    {
+        /// <summary>
+        /// default, does nothing special
+        /// </summary>
+        none = 0,
+        /// <summary>
+        /// the field values must be string providing image location (either local files path or url's) which are then used to render the elements thumbnails (on List view mode at least)
+        /// </summary>
+        Thumbnail,
+        /// <summary>
+        /// drops the need for generating guids using this instead
+        /// </summary>
+        UID,
+        /// <summary>
+        /// the main title shown in ListView cards
+        /// </summary>
+        Title,
+        /// <summary>
+        /// shown under the main title in ListView cards
+        /// </summary>
+        SubTitle,
+        /// <summary>
+        /// fields such as product's category, or music genre, allow UI features such as grouping and used in stats.
+        /// </summary>
+        GroupBy
+    }
+}
