05b2f03e87ede0590a20d00ac4ed95a21373d578
yassinMi <dir16cat17@gmail.com>
2022-08-20 14:55:05 +0100

    enhanced ui, selectable help examples..

--- scraper/ViewModel/CategoryPicker/CategoryPickerVM.cs
+++ scraper/ViewModel/CategoryPicker/CategoryPickerVM.cs
@@ -18,6 +18,56 @@ namespace scraper.ViewModel.CategoryPicker
                 new ExplorerFolder() {Name="sub 1" },
                  new ExplorerTarget() {Name="tar 1" },
             } });
+            Children.Add(new ExplorerFolder() { Name = "my folder 1" });
+            Children.Add(new ExplorerFolder() { Name = "my folder 1" });
+            Children.Add(new ExplorerFolder()
+            {
+                Name = "my folder 3",
+                Children = new List<ExplorerElement>() {
+                new ExplorerFolder() {Name="sub 1" },
+                 new ExplorerTarget() {Name="tar 1" },
+            }
+            });
+            Children.Add(new ExplorerFolder() { Name = "my folder 1" });
+            Children.Add(new ExplorerFolder() { Name = "my folder 1" });
+            Children.Add(new ExplorerFolder()
+            {
+                Name = "my folder 3",
+                Children = new List<ExplorerElement>() {
+                new ExplorerFolder() {Name="sub 1" },
+                 new ExplorerTarget() {Name="tar 1" },
+            }
+            });
+            Children.Add(new ExplorerFolder() { Name = "my folder 1" });
+            Children.Add(new ExplorerFolder() { Name = "my folder 1" });
+            Children.Add(new ExplorerFolder()
+            {
+                Name = "my folder 3",
+                Children = new List<ExplorerElement>() {
+                new ExplorerFolder() {Name="sub 1" },
+                 new ExplorerTarget() {Name="tar 1" },
+            }
+            });
+            Children.Add(new ExplorerFolder() { Name = "my folder 1" });
+            Children.Add(new ExplorerFolder() { Name = "my folder 1" });
+            Children.Add(new ExplorerFolder()
+            {
+                Name = "my folder 3",
+                Children = new List<ExplorerElement>() {
+                new ExplorerFolder() {Name="sub 1" },
+                 new ExplorerTarget() {Name="tar 1" },
+            }
+            });
+            Children.Add(new ExplorerFolder() { Name = "my folder 1" });
+            Children.Add(new ExplorerFolder() { Name = "my folder 1" });
+            Children.Add(new ExplorerFolder()
+            {
+                Name = "my folder 3",
+                Children = new List<ExplorerElement>() {
+                new ExplorerFolder() {Name="sub 1" },
+                 new ExplorerTarget() {Name="tar 1" },
+            }
+            });
         }
 
         public List<ExplorerElement> Children { get; set; } = new List<ExplorerElement>();
